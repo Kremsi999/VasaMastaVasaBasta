bedfbef8fd9429038e706a96305acca380fb2bb9
Nemanja Krcmar <nk.nemanjakrcmar@gmail.com>
2024-08-28 22:44:30 +0200

    Added reCAPTCHA to project

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,6 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RECAPTCHA_V3_SITE_KEY, RecaptchaV3Module, RecaptchaSettings } from 'ng-recaptcha';
+import { FormsModule } from '@angular/forms';
+import { environment } from 'src/environments/environment';
 
 @NgModule({
   declarations: [
@@ -10,9 +13,16 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RecaptchaV3Module,
+    FormsModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: RECAPTCHA_V3_SITE_KEY,
+      useValue: environment.recaptcha.siteKey,
+    },
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
